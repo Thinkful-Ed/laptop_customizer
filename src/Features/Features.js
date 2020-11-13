f1ddc8027b1013d02029769a829fff9ad59db9f5
Mayen Akpan <neroxma@yahoo.com>
2020-11-12 21:35:46 -0800

    added FeatureOption

--- src/Features/Features.js
+++ src/Features/Features.js
@@ -1,5 +1,6 @@
 import React from "react";
 import slugify from 'slugify';
+import FeatureOption from '../FeatureOption/FeatureOption';
 
 function Features(props){
     
@@ -7,21 +8,17 @@ function Features(props){
         const featureHash = feature + '-' + idx;
         const options = props.features[feature].map(item => {
           const itemHash = slugify(JSON.stringify(item));
-          return (
-            <div key={itemHash} className="feature__item">
-              <input
-                type="radio"
-                id={itemHash}
-                className="feature__option"
-                name={slugify(feature)}
-                checked={item.name === props.selected[feature].name}
-                onChange={e => props.updateFeature(feature, item)}
-              />
-              <label htmlFor={itemHash} className="feature__label">
-                {item.name} ({props.USCurrencyFormat.format(item.cost)})
-              </label>
-            </div>
-          );
+
+          return <FeatureOption 
+                   featureHash={featureHash}
+                   options={options}
+                   itemHash={itemHash}
+                   selected={props.selected}
+                   USCurrencyFormat={props.USCurrencyFormat}
+                   updateFeature={props.updateFeature}
+                   item={item}
+                   feature={feature}
+                 />
         });
   
         return (
