fdd2294ff21289c0ff858bd8ed45a1f96ec8530f
colburncodes <colburnsanders@gmail.com>
2020-12-28 12:00:25 -0800

    laptop customizer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React, { Component } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import ShoppingCartDisplay from "./components/ShoppingCart/ShoppingCartDisplay";
 import ShoppingCartTotal from "./components/ShoppingCart/ShoppingCartTotal";
-import features from "./components/Features/Features";
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from "slugify";
 
 import "./App.css";
+import LaptopItem from "./components/Laptop/LaptopItems";
 
 // This object will allow us to
 // easily convert numbers into US dollar values
@@ -48,45 +44,16 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + "-" + idx;
-      const options = this.props.features[feature].map((item) => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={(e) => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-
     return (
       <div className="App">
         <NavBar />
         <main>
-          <form className="main__form">
-            <h2>Customize your laptop</h2>
-            {features}
-          </form>
+          <LaptopItem
+            selected={this.state.selected}
+            features={this.props.features}
+            updateFeature={this.updateFeature}
+            USCurrencyFormat={USCurrencyFormat}
+          />
           <ShoppingCartDisplay
             selected={this.state.selected}
             USCurrencyFormat={USCurrencyFormat}
