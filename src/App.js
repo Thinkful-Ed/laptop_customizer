88bcd83dc92b8978b9efe9889ce94f8d2af50372
Rick Maya <rickmaya@gmail.com>
2021-02-18 16:28:25 -0600

    delete commented code

--- src/App.js
+++ src/App.js
@@ -73,21 +73,12 @@ class App extends Component {
     const summary = Object.keys(this.state.selected).map((feature, idx) => {
       const featureHash = feature + '-' + idx;
       const selectedOption = this.state.selected[feature];
-
-
       return (
         <SummaryOption
           key={featureHash}
           currencyFormat={USCurrencyFormat}
           selectedOption={selectedOption}
         />
-        // <div className="summary__option" key={featureHash}>
-        //   <div className="summary__option__label">{feature} </div>
-        //   <div className="summary__option__value">{selectedOption.name}</div>
-        //   <div className="summary__option__cost">
-        //     {USCurrencyFormat.format(selectedOption.cost)}
-        //   </div>
-        // </div>
       );
     });
 
